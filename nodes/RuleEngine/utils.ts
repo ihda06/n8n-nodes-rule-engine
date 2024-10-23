a1aa516d5c93869cfcad8671fd3553947b955bc5
Ihda Anwari <ihdaanwari5@gmail.com>
2024-10-23 15:09:38 +0700

    feat: node rule engine init using if node template

---
+++ nodes/RuleEngine/utils.ts
@@ -0,0 +1,27 @@
+import type { IExecuteFunctions, INodeProperties } from 'n8n-workflow';
+
+export const getTypeValidationStrictness = (version: number) => {
+	return `={{ ($nodeVersion < ${version} ? $parameter.options.looseTypeValidation :  $parameter.looseTypeValidation) ? "loose" : "strict" }}`;
+};
+
+export const getTypeValidationParameter = (version: number) => {
+	return (context: IExecuteFunctions, itemIndex: number, option: boolean | undefined) => {
+		if (context.getNode().typeVersion < version) {
+			return option;
+		} else {
+			return context.getNodeParameter('looseTypeValidation', itemIndex, false) as boolean;
+		}
+	};
+};
+
+export const looseTypeValidationProperty: INodeProperties = {
+	displayName: 'Convert types where required',
+	description:
+		'If the type of an expression doesn\'t match the type of the comparison, n8n will try to cast the expression to the required type. E.g. for booleans <code>"false"</code> or <code>0</code> will be cast to <code>false</code>',
+	name: 'looseTypeValidation',
+	type: 'boolean',
+	default: true,
+};
+
+export const ENABLE_LESS_STRICT_TYPE_VALIDATION =
+	"Try changing the type of comparison. Alternatively you can enable 'Convert types where required'.";
