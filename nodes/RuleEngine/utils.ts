7e1022238df5cd2af31a5bd0fef33d4690a9fa00
Ihda Anwari <ihdaanwari5@gmail.com>
2024-10-24 09:51:12 +0700

    update

--- nodes/RuleEngine/utils.ts
+++ nodes/RuleEngine/utils.ts
@@ -15,7 +15,9 @@ export const getTypeValidationParameter = (version: number) => {
 };
 
 export const looseTypeValidationProperty: INodeProperties = {
+	// eslint-disable-next-line
 	displayName: 'Convert types where required',
+	// eslint-disable-next-line
 	description:
 		'If the type of an expression doesn\'t match the type of the comparison, n8n will try to cast the expression to the required type. E.g. for booleans <code>"false"</code> or <code>0</code> will be cast to <code>false</code>',
 	name: 'looseTypeValidation',
